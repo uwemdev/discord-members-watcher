ac650be01cfcbcf6ee04cc060f728839bf8a70f1
uwemdev <uwemuwemetim@gmail.com>
2026-03-30 14:26:27 +0100

    Add startup summary of new members since last run

--- discord_telegram_monitor.py
+++ discord_telegram_monitor.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import requests
+import json
+import os
 
 # 1. Replace with your Discord user token (NOT your bot token)
 DISCORD_TOKEN = 'YOUR_DISCORD_USER_TOKEN'
@@ -12,6 +14,19 @@ TELEGRAM_CHANNEL = '@thediscordtelegram'
 
 bot = commands.Bot(command_prefix='!', self_bot=True)
 
+# File to store last known members
+MEMBERS_FILE = 'last_members.json'
+
+def load_last_members():
+    if os.path.exists(MEMBERS_FILE):
+        with open(MEMBERS_FILE, 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_last_members(data):
+    with open(MEMBERS_FILE, 'w') as f:
+        json.dump(data, f)
+
 def send_telegram_message(text):
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
     data = {
@@ -23,6 +38,24 @@ def send_telegram_message(text):
 
 # Send a test message on script start
 send_telegram_message("Test message from my bot!")
+
+# On startup, report new members since last run
+@bot.event
+async def on_ready():
+    last_members = load_last_members()
+    new_last_members = {}
+    for guild in bot.guilds:
+        await guild.chunk()  # Ensure all members are loaded
+        current_ids = set(str(m.id) for m in guild.members)
+        new_last_members[str(guild.id)] = list(current_ids)
+        prev_ids = set(last_members.get(str(guild.id), []))
+        new_ids = current_ids - prev_ids
+        if new_ids:
+            new_users = [m for m in guild.members if str(m.id) in new_ids]
+            msg = f"<b>New members since last run in {guild.name}:</b>\n"
+            msg += '\n'.join(f"<code>{u.name}#{u.discriminator}</code>" for u in new_users)
+            send_telegram_message(msg)
+    save_last_members(new_last_members)
 
 # Optional: Map server IDs to invite links
 SERVER_INVITES = {
